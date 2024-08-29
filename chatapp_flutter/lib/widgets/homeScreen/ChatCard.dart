05c4c33353284285a1b6ccddf840f4c027320a68
AayushPandeyy <ap200061@gmail.com>
2024-08-29 16:13:21 +0545

    Text Overflow Bug fixed

--- chatapp_flutter/lib/widgets/homeScreen/ChatCard.dart
+++ chatapp_flutter/lib/widgets/homeScreen/ChatCard.dart
@@ -4,23 +4,24 @@ class ChatCard extends StatelessWidget {
   final String username;
   final String latestMessage;
   final String profilePictureUrl;
-  const ChatCard(
-      {super.key,
-      required this.username,
-      required this.latestMessage,
-      required this.profilePictureUrl});
+
+  const ChatCard({
+    super.key,
+    required this.username,
+    required this.latestMessage,
+    required this.profilePictureUrl,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      
-      width: 100,
-      // height: 80,
+      width: MediaQuery.sizeOf(context).width,
       decoration: const BoxDecoration(
-          color: Color(0xff34495E),
-          border: Border.symmetric(
-              horizontal:
-                  BorderSide(color: Color.fromARGB(255, 222, 217, 217)))),
+        color: Color(0xff34495E),
+        border: Border.symmetric(
+          horizontal: BorderSide(color: Color.fromARGB(255, 222, 217, 217)),
+        ),
+      ),
       child: Padding(
         padding: const EdgeInsets.all(15.0),
         child: Row(
@@ -29,32 +30,34 @@ class ChatCard extends StatelessWidget {
               radius: 25,
               foregroundImage: NetworkImage(profilePictureUrl),
             ),
-            SizedBox(
-              width: 20,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              
-              children: [
-                Text(
-                  username,
-                  style: const TextStyle(
+            SizedBox(width: 20),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    username,
+                    style: const TextStyle(
                       fontFamily: "MarkoOne",
                       fontWeight: FontWeight.bold,
                       fontSize: 30,
-                      color: Color(0xffF4D03F)),
-                ),
-                Text(
-                  latestMessage,
-                  style: const TextStyle(
+                      color: Color(0xffF4D03F),
+                    ),
+                    overflow: TextOverflow.ellipsis, // Handle overflow
+                  ),
+                  Text(
+                    latestMessage,
+                    style: const TextStyle(
                       fontFamily: "UbuntuMono",
-                      // fontWeight: FontWeight.bold,
                       fontSize: 20,
-                      color: Colors.white),
-                )
-              ],
-            )
+                      color: Colors.white,
+                    ),
+                    overflow: TextOverflow.ellipsis, // Handle overflow
+                    maxLines: 1, // Limit to one line
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
